3ccb505cb78ad2a8d82597fe245b4068d19f65dd
Vitor Faiante <vitorf7@gmail.com>
2015-12-17 12:46:59 +0000

    - Finish day 3 puzzle 2

--- spec/AdventOfCode/Day3/ElfGuideSpec.php
+++ spec/AdventOfCode/Day3/ElfGuideSpec.php
@@ -13,64 +13,99 @@ class ElfGuideSpec extends ObjectBehavior
         $this->beConstructedWith($santa);   
     }
 
-    // function it_is_initializable()
-    // {
-    //     $this->shouldHaveType('VFDev\AdventOfCode\Day3\ElfGuide');
-    // }
-
-    // public function it_should_be_able_to_tell_santa_to_move_north_with_a_carret_symbol(Santa $santa)
-    // {
-    //     $santa->moveNorth()->shouldBeCalled();
-
-    //     $this->readDirections('^');
-    // }
-
-    // public function it_should_tell_santa_to_move_south_with_a_v_symbol(Santa $santa)
-    // {
-    //     $santa->moveSouth()->shouldBeCalled();
-
-    //     $this->readDirections('v');
-    // }
-
-    // public function it_should_be_able_to_tell_santa_to_move_west_with_a_minus_than_symbol(Santa $santa)
-    // {
-    //     $santa->moveWest()->shouldBeCalled();
-
-    //     $this->readDirections('<');
-    // }
-
-    // public function it_should_be_able_to_tell_santa_to_move_east_with_a_greater_than_symbol(Santa $santa)
-    // {
-    //     $santa->moveEast()->shouldBeCalled();
-
-    //     $this->readDirections('>');
-    // }
-
-    // public function it_should_be_able_to_tell_santa_to_move_multiple_times_with_more_than_one_direction(Santa $santa)
-    // {
-    //     $this->readDirections('^v');
-
-    //     $santa->moveNorth()->shouldBeCalledTimes(1);
-    //     $santa->moveSouth()->shouldBeCalledTimes(1);
-    // }
-
-    // public function it_should_be_able_to_tell_santa_to_move_in_all_directions_when_give_the_directions_to_do_so(Santa $santa)
-    // {
-    //     $this->readDirections('^>v<');
-
-    //     $santa->moveNorth()->shouldBeCalledTimes(1);
-    //     $santa->moveEast()->shouldBeCalledTimes(1);
-    //     $santa->moveSouth()->shouldBeCalledTimes(1);
-    //     $santa->moveWest()->shouldBeCalledTimes(1);
-    // }
-
-    // public function it_throws_an_exception_error_when_a_direction_is_not_found()
-    // {
-    //     $this->shouldThrow('\Exception')->during('readDirections', array('}'));
-    // }
-
-    public function it_should_be_able_to_have_many_santas_passed_in(Santa $santa1, Santa $santa2)
+    function it_is_initializable()
     {
-        $this->beConstructedWith([$santa1, $santa2]);
+        $this->shouldHaveType('VFDev\AdventOfCode\Day3\ElfGuide');
+    }
+
+    public function it_should_be_able_to_tell_santa_to_move_north_with_a_carret_symbol(Santa $santa)
+    {
+        $santa->moveNorth()->shouldBeCalled();
+
+        $this->readDirections('^');
+    }
+
+    public function it_should_tell_santa_to_move_south_with_a_v_symbol(Santa $santa)
+    {
+        $santa->moveSouth()->shouldBeCalled();
+
+        $this->readDirections('v');
+    }
+
+    public function it_should_be_able_to_tell_santa_to_move_west_with_a_minus_than_symbol(Santa $santa)
+    {
+        $santa->moveWest()->shouldBeCalled();
+
+        $this->readDirections('<');
+    }
+
+    public function it_should_be_able_to_tell_santa_to_move_east_with_a_greater_than_symbol(Santa $santa)
+    {
+        $santa->moveEast()->shouldBeCalled();
+
+        $this->readDirections('>');
+    }
+
+    public function it_should_be_able_to_tell_santa_to_move_multiple_times_with_more_than_one_direction(Santa $santa)
+    {
+        $this->readDirections('^v');
+
+        $santa->moveNorth()->shouldBeCalledTimes(1);
+        $santa->moveSouth()->shouldBeCalledTimes(1);
+    }
+
+    public function it_should_be_able_to_tell_santa_to_move_in_all_directions_when_give_the_directions_to_do_so(Santa $santa)
+    {
+        $this->readDirections('^>v<');
+
+        $santa->moveNorth()->shouldBeCalledTimes(1);
+        $santa->moveEast()->shouldBeCalledTimes(1);
+        $santa->moveSouth()->shouldBeCalledTimes(1);
+        $santa->moveWest()->shouldBeCalledTimes(1);
+    }
+
+    public function it_throws_an_exception_error_when_a_direction_is_not_found()
+    {
+        $this->shouldThrow('\Exception')->during('readDirections', array('}'));
+    }
+
+    public function it_should_be_able_to_have_many_santas_passed_in(Santa $santa1, Santa $roboSanta)
+    {
+        $this->beConstructedWith([$santa1, $roboSanta]);
+
+        $this->getSantas()->shouldBeArray();
+    }
+
+    public function it_should_give_directions_to_each_of_two_santas_alternatively(Santa $santa1, Santa $roboSanta)
+    {
+        $this->beConstructedWith([$santa1, $roboSanta]);
+
+        $this->readDirections('^v');
+
+        $santa1->moveNorth()->shouldBeCalledTimes(1);
+        $roboSanta->moveSouth()->shouldBeCalledTimes(1);
+    }
+
+    public function it_should_give_directions_to_each_santa_alternatively(Santa $santa1, Santa $roboSanta)
+    {
+        $this->beConstructedWith([$santa1, $roboSanta]);
+
+        $this->readDirections('^v<>');
+
+        $santa1->moveNorth()->shouldBeCalledTimes(1);
+        $roboSanta->moveSouth()->shouldBeCalledTimes(1);
+        $santa1->moveWest()->shouldBeCalledTimes(1);
+        $roboSanta->moveEast()->shouldBeCalledTimes(1);
+    }
+
+    public function it_should_be_able_to_tell_3_santas_their_directions_alternatively(Santa $santa1, Santa $santa2, Santa $santa3)
+    {
+        $this->beConstructedWith([$santa1, $santa2, $santa3]);
+
+        $this->readDirections('^v<');
+
+        $santa1->moveNorth()->shouldBeCalledTimes(1);
+        $santa2->moveSouth()->shouldBeCalledTimes(1);
+        $santa3->moveWest()->shouldBeCalledTimes(1);
     }
 }
--- src/AdventOfCode/Day3/ElfGuide.php
+++ src/AdventOfCode/Day3/ElfGuide.php
@@ -6,7 +6,7 @@ use VFDev\AdventOfCode\Day3\Santa;
 
 class ElfGuide
 {
-    protected $santa;
+    protected $santas = [];
 
     protected $directionsLookup = array(
         '^' => 'moveNorth',
@@ -15,19 +15,38 @@ class ElfGuide
         '>' => 'moveEast'
     );
 
-    public function __construct(Santa $santa)
+    public function __construct($santas)
     {
-        $this->santa = $santa;
-        var_dump($santa);
+        $this->santas = $santas;
     }
 
     public function readDirections($directions)
     {
-        foreach (str_split($directions) as $direction) {
-            $directionMethodName = $this->getDirectionMethodName($direction);
-
-            $this->santa->{$directionMethodName}();   
+        if (!is_array($this->santas)) {
+            foreach (str_split($directions) as $direction) {
+                $this->tellSantaToMove($direction, $this->santas);
+            }   
+        } else {
+            $this->readDirectionsAlternatively($directions);
         }
+    }
+
+    private function readDirectionsAlternatively($directions)
+    {
+        foreach (str_split($directions) as $direction) {
+            $current_santa = (isset($current_santa) && $current_santa <= count($this->santas) - 1) ? $current_santa : 0;
+
+            $this->tellSantaToMove($direction, $this->santas[$current_santa]); 
+
+            $current_santa++;
+        }
+    }
+
+    private function tellSantaToMove($direction, Santa $santa)
+    {
+        $directionMethodName = $this->getDirectionMethodName($direction);
+
+        $santa->{$directionMethodName}();
     }
 
     private function getDirectionMethodName($direction)
@@ -37,5 +56,10 @@ class ElfGuide
         }
 
         throw new \Exception('Sorry Direction could not be found!');
+    }
+
+    public function getSantas()
+    {
+        return $this->santas;
     }
 }
